3f9ff112bbbd9f105f54d9f0475f3fd999bf03f5
laplai_m <laplai_m@samar07.cnam.fr>
2015-12-11 10:57:14 +0100

    + ajout test

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: laplai_m
+ *
+ * Created on 11 décembre 2015, 10:30
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printr("lol");
+    return (EXIT_SUCCESS);
+}
+
